c42d1878c916c29f24794841840b64280c13999c
nomuyoshi <nomuyoshi0111@gmail.com>
2020-02-02 01:54:05 +0900

    prefixで処理を分ける

--- handler.go
+++ handler.go
@@ -3,9 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
+
+// PREFIX はリマインド登録に必要なキーワード
+const PREFIX = "リマインド"
+
+// USAGE は使い方説明。
+const USAGE = `「リマインド　内容」と話しかけてください。
+例) リマインド　食パンを買って帰る`
 
 // CallbackHandler はLINEからのcallbackを処理する構造体
 type CallbackHandler struct {
@@ -22,12 +30,23 @@ func (h *CallbackHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
-			replyToken := event.ReplyToken
 			var messages []linebot.SendingMessage
-			message := linebot.NewTextMessage("受け付けました！！")
-			messages = append(messages, message)
 
-			if _, err := h.bot.ReplyMessage(replyToken, messages...).Do(); err != nil {
+			switch receivedMessage := event.Message.(type) {
+			case *linebot.TextMessage:
+				receivedText := strings.TrimSpace(receivedMessage.Text)
+
+				if strings.HasPrefix(receivedText, PREFIX) {
+					content := strings.TrimSpace(strings.Replace(receivedText, PREFIX, "", 1))
+					messages = append(messages, linebot.NewTextMessage("受け付けました！！\n"+content))
+				} else {
+					messages = append(messages, linebot.NewTextMessage(USAGE))
+				}
+			default:
+				messages = append(messages, linebot.NewTextMessage(USAGE))
+			}
+
+			if _, err := h.bot.ReplyMessage(event.ReplyToken, messages...).Do(); err != nil {
 				log.Print("Error reply: ", err)
 			}
 		}
